9cfd53aaa10be6447d08d741aaa53e024cfd7da9
PlacaFromHell <40618535+PlacaFromHell@users.noreply.github.com>
2022-05-03 04:08:45 -0300

    Add files via upload

---
+++ Tarea array/Workspace/index.js
@@ -0,0 +1,22 @@
+let condicionBucle = "sí";
+let i = 0;
+
+const contadorAlert = function(i) { return i + 1 };
+
+const lista = [];
+
+alert("El siguiente programa crea una lista de artículos. A continuación introduzca el primer artículo:");
+
+while(condicionBucle=="sí") {
+    lista[i] = prompt("Artículo Nº" + contadorAlert(i));
+    i++;
+
+    do { //validación
+    condicionBucle = prompt("¿Vas a añadir otro elemento? (sí, no):");
+    condicionBucle = condicionBucle.toLowerCase();
+    }
+    while (condicionBucle!="sí" && condicionBucle!="no")
+
+}
+
+lista.forEach(lista => console.log(lista));
